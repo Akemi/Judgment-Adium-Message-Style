220f98db0466148eec579303fcec9e6dae3db922
Akemi <Akemi@Akemis-MacPro.fritz.box>
2015-05-01 16:42:03 +0200

    fixed irc spoilers with hack, fix inline image preview, fix long sender names

--- Judgment.AdiumMessageStyle/Contents/Resources/custom.js
+++ Judgment.AdiumMessageStyle/Contents/Resources/custom.js
@@ -1,30 +1,61 @@
 $( document ).ready(function() {
 
     function imagePreview(url) {
-        $("<img>", {
+        $('<img>', {
             src: url,
             error: function() {
 
             },
             load: function() {
-                var ele = $('#preview_image')
-                ele.css('background-image', 'url(' + url + ')');
-                ele.stop()
-                ele.show( "slow" );
+                $('<img/>').attr('src', url).load(function() {
+                    $(this).remove();
+                    var ele = $('#preview_image');
+                    ele.show();
+                    ele.css('background-image', 'url(' + url + ')');
+                });
             }
         });
     }
 
-    $( "#Chat" ).on("mouseenter", "a", function() {
-        imagePreview($(this).attr('href'));
+    function changeColor(ele) {
+        var colorBG = ele.attr('back');
+        var colorText = ele.attr('color');
+        if(typeof colorBG !== 'undefined') {
+            ele.css('background-color', colorBG);
+        }
+        if(colorText == '#00ac00') {
+            ele.css('color', '#c0c0c0');
+            ele.css('background-color', '#c0c0c0');
+            ele.addClass('spoiler');
+        }
+    }
+
+    $('#Chat').on('mouseenter', 'a', function() {
+        var url = $(this).attr('href');
+        var ext = url.split('.').pop();;
+        if(ext != 'pdf') imagePreview(url);
     });
 
-    $( "#Chat" ).on("mouseleave", "a", function() {
-        var ele = $('#preview_image')
-        ele.stop()
-        ele.hide( "hide" , function() {
-            ele.css('background-image', '');
+    $('#Chat').on('mouseleave', 'a', function() {
+        var ele = $('#preview_image');
+        ele.hide();
+    });
+
+    $('#preview_image').on('mouseleave', function() {
+        $(this).hide();
+    });
+
+    $('#Chat').bind('DOMNodeInserted DOMNodeRemoved', function(event) {
+        console.log(event.target.innerHTML);
+        var ele = $(event.target).find('.message_content > font');
+        $.each(ele, function(key, value) {
+            changeColor($(this));
         });
+    });
+
+    var ele = $('#Chat > .message_wrapper > .message_content > font');
+    $.each(ele, function(key, value) {
+        changeColor($(this));
     });
 
 });
